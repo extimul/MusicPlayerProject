9ac081bda18551d69c97edfac4eff2a0bb51d706
ExTimul <APlay819@gmail.com>
2021-06-14 17:07:20 +0300

    Added slider style and music control buttons

---
+++ Core/Helpers/Converters/ContentConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MusicPlayerProject.Core.Helpers.Converters
+{
+    public class ContentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DrawingGroup draw = (value as DrawingBrush).Drawing as DrawingGroup;
+            PathGeometry path = new PathGeometry();
+
+            foreach (GeometryDrawing item in draw.Children)
+            {
+                path.AddGeometry(item.Geometry);
+            }
+
+            return path;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ Core/Helpers/Converters/ScaleConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MusicPlayerProject.Core.Helpers.Converters
+{
+    public class ScaleConverter : IValueConverter
+    {
+        public double Scale { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double num = (double)value;
+            return (num * (Scale / 100));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
