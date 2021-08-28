0dbb46f7b0d1203e2bb5f7f122564efef9961bea
extimul <APlay819@gmail.com>
2021-08-28 23:18:16 +0300

    -

---
+++ MusicPlayer.App.WPF/ViewModels/Base/CollectionViewModel.cs
@@ -0,0 +1,33 @@
+﻿using MusicPlayer.App.WPF.ViewModels.Controls;
+using MusicPlayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MusicPlayer.App.WPF.ViewModels.Base
+{
+    public abstract class CollectionViewModel : ViewModelBase
+    {
+        public PlaylistControlBarViewModel ControlBarViewModel { get; set; }
+
+        public virtual Track SelectedTrack { get; set; }
+
+        public virtual Playlist CurrentPlaylist { get; set; }
+
+        public abstract DrawingBrush PlayPauseIcon { get; }
+
+        public abstract ObservableCollection<Track> TracksCollection { get; }
+
+        public abstract ObservableCollection<MenuItemObject> ContextMenuItems { get; set; }
+
+        public abstract ICommand PlayPauseCommand { get; set; }
+        public abstract ICommand ContextMenuCommand { get; set; }
+
+        public abstract void LoadContextMenuItems();
+    }
+}
